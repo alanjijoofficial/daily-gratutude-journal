411064df2c9365c11ffa31ccaeeeb421b06f9457
alan <alanjijoofficial@gmail.com>
2026-08-16 22:48:01 +0530

    Fix domain typo in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,7 +139,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3001',
-    'https://daily-gratutude-journal.vercel.app',
+    'https://daily-gratitide-journal.vercel.app',
     
 ]
 
