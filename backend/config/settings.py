46026497de1dfaae05cce07db16c267fc124b918
alan <alanjijoofficial@gmail.com>
2026-08-17 12:07:59 +0530

    Fix CSRF_TRUSTED_ORIGINS syntax error

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,6 +28,9 @@ ALLOWED_HOSTS = [
     for host in os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost,*').split(',')
     if host.strip()
 ]
+CSRF_TRUSTED_ORIGINS = [
+    ' https://daily-gratutude-journal.onrender.com',
+]
 
 
 # Application definition
