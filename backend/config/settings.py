d5df0839e957fefc41c4c8dc14671c6a8cdafda3
alan <alanjijoofficial@gmail.com>
2026-08-17 11:17:25 +0530

    Fix trailing slash in CORS_ALLOWED_ORIGINS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -133,7 +133,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://daily-gratutude-journal-git-main-alan-jijo.vercel.app/'
+    'https://daily-gratutude-journal-git-main-alan-jijo.vercel.app'
 ]
 CORS_ALLOW_CREDENTIALS = True
 
